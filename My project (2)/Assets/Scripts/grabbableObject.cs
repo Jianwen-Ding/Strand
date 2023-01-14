e1cd1093c826be475adbbd66f2bb0a0e4be66020
Pakmanlie <62767966+Pakmanlie@users.noreply.github.com>
2023-01-13 22:56:56 -0500

    1/13/2023 grab object start

---
+++ My project (2)/Assets/Scripts/grabbableObject.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grabbableObject : MonoBehaviour
+{
+    [SerializeField]
+    private bool angleChange;
+    [SerializeField]
+    private float grabShrink;
+    [SerializeField]
+    private float throwStrength;
+    [SerializeField]
+    private bool hasBeenGrabbed;
+    public bool getHasBeenGrabbed()
+    {
+        return hasBeenGrabbed;
+    }
+    public void setHasBeenGrabbed(bool set)
+    {
+        hasBeenGrabbed = set;
+    }
+    public virtual void grabbed()
+    {
+        
+    }
+    public virtual void 
+    public virtual void released()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
