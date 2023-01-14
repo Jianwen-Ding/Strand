d7badbfc8904052a748d634cf09855e56a71fa1a
Pakmanlie <62767966+Pakmanlie@users.noreply.github.com>
2023-01-14 16:57:29 -0500

    1/14/2023 editing layering

--- My project (2)/Assets/Scripts/SpriteYLayering.cs
+++ My project (2)/Assets/Scripts/SpriteYLayering.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class SpriteYLayering : MonoBehaviour
 {
+    //This script makes it so that there are not any layering/perspective errors
+    [SerializeField]
+    private bool unmoving;
+    [SerializeField]
+    private float yAdjust;
     private SpriteRenderer objectSpriteRender;
     // Start is called before the first frame update
     void Start()
     {
         objectSpriteRender = gameObject.GetComponent<SpriteRenderer>();
+        if (unmoving)
+        {
+            objectSpriteRender.sortingOrder = -(int)(50 * (gameObject.transform.position.y + yAdjust));
+            enabled = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        objectSpriteRender.sortingOrder = -(int)gameObject.transform.position.y;
+        objectSpriteRender.sortingOrder = -(int)(50 * (gameObject.transform.position.y + yAdjust));
     }
 }
