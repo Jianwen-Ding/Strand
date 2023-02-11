3f39407fa8c1a726ff5360f72561ea670bc64654
Pakmanlie <62767966+Pakmanlie@users.noreply.github.com>
2023-02-11 12:34:16 -0500

    2/11/2023 beginning on overall grid generation

---
+++ My project (2)/Assets/Scripts/Map Loader/girdOverallLoader.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class girdOverallLoader : MonoBehaviour
+{
+    //Player Locations 
+    int playerLocationX;
+    int playerLocationY;
+    //Grid Set
+    singleGridPageLoader[][] pageGridMap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- My project (2)/Assets/Scripts/Map Loader/singleGridPageLoader.cs
+++ My project (2)/Assets/Scripts/Map Loader/singleGridPageLoader.cs
@@ -32,25 +32,76 @@ public class singleGridPageLoader : MonoBehaviour
     private int[][] pageMap;
     //key for integer map
     private GameObject[] pageKey;
+    //Determines if grid has been generated yet
+    [SerializeField]
+    private bool generatedYet = false;
+    //Determines if grid has been loaded in
+    [SerializeField]
+    private bool loadedIn = false;
     //-- parameters to load and unload with based on distance--
     ArrayList loadedObject = new ArrayList();
+    //Get/Set Variable
+    public bool getUpOpen()
+    {
+        return upOpen;
+    }
+    public void setUpOpen(bool set)
+    {
+        upOpen = set;
+    }
+    public bool getDownOpen()
+    {
+        return downOpen;
+    }
+    public void setDownOpen(bool set)
+    {
+        downOpen = set;
+    }
+    public bool getRightOpen()
+    {
+        return rightOpen;
+    }
+    public void setRightOpen(bool set)
+    {
+        rightOpen = set;
+    }
+    public bool getLeftOpen()
+    {
+        return leftOpen;
+    }
+    public void setLeftOpen(bool set)
+    {
+        leftOpen = set;
+    }
+    public void generateGrid()
+    {
+        if (!generatedYet)
+        {
+            generatedYet = true;
+            //gets needed variables
+            pageMap = gridPageStorer.findRandomSuitablePage(upOpen, downOpen, leftOpen, rightOpen, pageTheme, pageSpecialUse).getMap();
+            pageKey = gridPageStorer.getGridKey();
+            //loads page
+            for (int y = 0; y < pageMap.Length; y++)
+            {
+                for (int x = 0; x < pageMap[y].Length; x++)
+                {
+                    GameObject newObject = Instantiate(pageKey[pageMap[y][x]], new Vector3(initialDisplacement.x + gameObject.transform.position.x + gridXDistance * x, initialDisplacement.y + gameObject.transform.position.y + gridYDistance * y), Quaternion.identity.normalized);
+                    loadedObject.Add(newObject);
+                }
+            }
+        }
+        else
+        {
+            print("ERROR- THIS GRID SEEMS TO HAVE ALREADY BEEN GENERATED BUT FUNCTION -generateGrid- HAS BEEN CALLED AGAIN");
+        }
+        
+    }
     // Start is called before the first frame update
     void Start()
     {
-        //gets needed variables
-        pageMap = gridPageStorer.findRandomSuitablePage(upOpen, downOpen, leftOpen, rightOpen, pageTheme, pageSpecialUse).getMap();
-        pageKey = gridPageStorer.getGridKey();
-        //loads page
-        for(int y = 0; y < pageMap.Length; y++)
-        {
-            for(int x = 0; x < pageMap[y].Length; x++)
-            {
-                GameObject newObject = Instantiate(pageKey[pageMap[y][x]], new Vector3(initialDisplacement.x + gameObject.transform.position.x + gridXDistance * x, initialDisplacement.y + gameObject.transform.position.y + gridYDistance * y), Quaternion.identity.normalized);
-                loadedObject.Add(newObject);
-            }
-        }
+   
     }
-
     // Update is called once per frame
     void Update()
     {
