829a33a116bffc50907264ec11ce88fbfae1cec8
Pakmanlie <62767966+Pakmanlie@users.noreply.github.com>
2023-07-14 17:29:18 -0400

    wow

--- My project (2)/Assets/Scripts/InGame/Audio/backGroundThemeSystem.cs
+++ My project (2)/Assets/Scripts/InGame/Audio/backGroundThemeSystem.cs
@@ -36,7 +36,13 @@ public class backGroundThemeSystem : MonoBehaviour
     [SerializeField]
     AudioClip nightThemeMuffled;
     [SerializeField]
+    float nightmMuffledVolume;
+    [SerializeField]
     AudioClip transitionBell;
+    [SerializeField]
+    float bellVolume;
+    //variables
+    //to swap between muffled audio clips and non muffled ones
     [SerializeField]
     bool isMuffled;
     //state starts from 0 and goes up as each song ends
@@ -46,36 +52,45 @@ public class backGroundThemeSystem : MonoBehaviour
     //2 - dusk
     //3 - night
     int dayState;
+    //manages changes into next state
+    float timeLeftUntilNextState;
     //Cache variable
     AudioSource soundSource;
     // Start is called before the first frame update
     void Start()
     {
+        timeLeftUntilNextState = dawnTheme.length;
         dayState = 0;
         isMuffled = false;
         soundSource = gameObject.GetComponent<AudioSource>();
     }
-    public AudioClip findTheme(int indexSet, bool muffleSet)
+    public void playThemeFromIndex(int indexSet, bool muffleSet)
     {
         AudioClip returnTheme;
+        float returnVolume;
         if (muffleSet)
         {
             switch (indexSet)
             {
                 case 0:
                     returnTheme = dawnThemeMuffled;
+                    returnVolume = dawnMuffledVolume;
                     break;
                 case 1:
                     returnTheme = middayThemeMuffled;
+                    returnVolume = middayMuffledVolume;
                     break;
                 case 2:
                     returnTheme = duskThemeMuffled;
+                    returnVolume = duskMuffledVolume;
                     break;
                 case 3:
                     returnTheme = nightThemeMuffled;
+                    returnVolume = nightmMuffledVolume;
                     break;
                 default:
                     returnTheme = null;
+                    returnVolume = 0;
                     print("ERROR- index given goes past parameters");
                     break;
             }
@@ -86,23 +101,30 @@ public class backGroundThemeSystem : MonoBehaviour
             {
                 case 0:
                     returnTheme = dawnTheme;
+                    returnVolume = dawnVolume;
                     break;
                 case 1:
                     returnTheme = middayTheme;
+                    returnVolume = middayVolume;
                     break;
                 case 2:
                     returnTheme = duskTheme;
+                    returnVolume = duskVolume;
                     break;
                 case 3:
                     returnTheme = nightTheme;
+                    returnVolume = nightVolume;
                     break;
                 default:
                     returnTheme = null;
+                    returnVolume = 0;
                     print("ERROR- index given goes past parameters");
                     break;
             }
         }
-        return returnTheme;
+        soundSource.volume = returnVolume;
+        soundSource.PlayOneShot(returnTheme);
+        
     }
     //Toggles wether to switch to unmuffled or muffled 
     public void muffelToggle(bool toggleSet)
@@ -110,13 +132,13 @@ public class backGroundThemeSystem : MonoBehaviour
         if(toggleSet != isMuffled)
         {
             float currentPlayBackTime = soundSource.time;
-            soundSource.PlayOneShot(findTheme(dayState, isMuffled));
+            playThemeFromIndex(dayState, toggleSet);
             soundSource.time = currentPlayBackTime;
         }
     }
     // Update is called once per frame
     void Update()
     {
-        //if(soundSource.)
+        
     }
 }
